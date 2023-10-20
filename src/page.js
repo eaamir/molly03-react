e16a80e019a10162bc675f4e9d3201d59ea596aa
mahbod <mahbodmo@outlook.com>
2023-10-20 14:47:48 +0330

    preloader added

--- src/page.js
+++ src/page.js
@@ -1,3 +1,6 @@
+import { useEffect, useState } from "react";
+import mollylg from "./img/MOLLY.png";
+
 import Footer from "./components/Footer";
 import SectionEight from "./components/SectionEight";
 import SectionFive from "./components/SectionFive";
@@ -10,8 +13,63 @@ import SectionTwo from "./components/SectionTwo";
 import Header from "./components/partials/Header";
 
 const Home = () => {
+  const [loading, setLoading] = useState(true);
+  const [textLoading, setTextLoading] = useState(true);
+  const [loadText, setLoadText] = useState("");
+
+  useEffect(() => {
+    document.body.style.overflow = "hidden";
+    setTimeout(() => {
+      setTextLoading(false);
+    }, 2000);
+    setTimeout(() => {
+      setLoading(false);
+      document.body.style.overflow = "visible";
+    }, 5000);
+    if (textLoading === false) {
+      if (loadText === "") {
+        setLoadText("M");
+      }
+      setTimeout(() => {
+        if (loadText === "M") {
+          setLoadText("MO");
+        }
+      }, 400);
+      setTimeout(() => {
+        if (loadText === "MO") {
+          setLoadText("MOL");
+        }
+      }, 400);
+      setTimeout(() => {
+        if (loadText === "MOL") {
+          setLoadText("MOLL");
+        }
+      }, 400);
+      setTimeout(() => {
+        if (loadText === "MOLL") {
+          setLoadText("MOLLY");
+        }
+      }, 400);
+    }
+  }, [loadText, textLoading]);
+
   return (
     <>
+      {loading && (
+        <div className="h-full w-full absolute z-50 bg-black text-white text-[40px]">
+          <div className="absolute top-[50%] left-[50%] mt-[-20px] ml-[-120px] flex items-center">
+            <img
+              loading="lazy"
+              src={mollylg}
+              width={65}
+              height={65}
+              alt="logo of molly website"
+              style={{ marginRight: "10px" }}
+            />
+            {loadText}
+          </div>
+        </div>
+      )}
       <main>
         <section id="sec01" className=" min-h-screen">
           <Header />
